ecb4bcf61d511beb567a7388514f79d68997323b
navid ashraf <ashraf.navid71@gmail.com>
2022-05-06 21:23:06 +0430

    Initialize the project

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const { ModuleFilenameHelpers } = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
